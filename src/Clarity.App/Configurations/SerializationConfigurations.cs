4c8c3d723f713ef67ea942d6dd71258f616ba7f3
gm50x <getuliomagela@outlook.com>
2026-08-13 21:15:53 -0300

    Add project files.

---
+++ src/Clarity.App/Configurations/SerializationConfigurations.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MvcJsonOptions = Microsoft.AspNetCore.Mvc.JsonOptions;
+using MvcOptions = Microsoft.AspNetCore.Mvc.MvcOptions;
+
+namespace Clarity.App.Configurations;
+
+public static class SerializationConfigurations
+{
+    private static readonly JsonNamingPolicy _jsonNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+    private static readonly JsonStringEnumConverter _jsonStringEnumConverter = new();
+    public static IServiceCollection SetupSerialization(this IServiceCollection services)
+    {
+        services.Configure<MvcJsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = _jsonNamingPolicy;
+            options.JsonSerializerOptions.Converters.Add(_jsonStringEnumConverter);
+        });
+
+        services.Configure<JsonOptions>(options =>
+        {
+            options.SerializerOptions.PropertyNamingPolicy = _jsonNamingPolicy;
+            options.SerializerOptions.Converters.Add(_jsonStringEnumConverter);
+        });
+
+        services.Configure<MvcOptions>(options =>
+        {
+            var transformer = new EndpointNamingTransformer();
+            var convention = new RouteTokenTransformerConvention(transformer);
+            options.Conventions.Add(convention);
+        });
+
+        return services;
+    }
+    private class EndpointNamingTransformer : IOutboundParameterTransformer
+    {
+        private static readonly JsonNamingPolicy _endpointNamingPolicy = JsonNamingPolicy.KebabCaseLower;
+        public string? TransformOutbound(object? value)
+        {
+            if (value == null) return null;
+            return _endpointNamingPolicy.ConvertName(value.ToString()!);
+        }
+    }
+}
